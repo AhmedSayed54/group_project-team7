502def99232727433a81b35ac26f0863ffe263d0
AhmedSayed54 <51422087+AhmedSayed54@users.noreply.github.com>
2021-08-15 23:51:32 +0200

    final

--- final project - team 7 (2).py
+++ final project - team 7 (2).py
@@ -1,36 +1,31 @@
+# Initializing variables
 import pygame
 from random import randint
 pygame.init()
 black = (0, 0, 0)
 white = (255, 255, 255)
 red = (255 , 0, 0)
+green = (0 , 255, 0)
 height = 800
 width = 800
 blue = (0, 110, 255)
-X_cat = 0
-Y_cat = 0
-x_cm = 0
-y_cm = 0
+X_cat = (randint(1,5)*82)+108
+Y_cat = (randint(1,5)*82)+108
+X_m = (randint(1,5)*82)+108
+Y_m = (randint(1, 5) * 82) + 108
 grey = (190, 190, 190)
-pygame.display.set_caption('Mouse and cat')  
 display_surface = pygame.display.set_mode((height, width))
-
-
-def position(x_cm, y_cm):
-    x_cm = (randint(1,5)*82)+108
-    y_cm = (randint(1,5)*82)+108
-    pygame.draw.rect(display_surface, red, [x_cm, y_cm, 80, 80])
-def delay():
-    pygame.time.delay(500)  
+pygame.display.set_caption('mouse and cat')
 def Draw_m():
-    pygame.draw.rect(display_surface, grey, [x_cm, y_cm, 80, 80])
+    pygame.draw.rect(display_surface, grey, [X_m, Y_m, 80, 80])
+def delay():
+    pygame.time.delay(100)
 def background():
     display_surface.fill(black)
     pygame.draw.rect(display_surface, white, [100, 100, 605, 605])
     x = 107
     y = 107
     i = 0
-
 
     display_surface.fill(black)
     pygame.draw.rect(display_surface, blue, [x, y, 80, 80])
@@ -61,43 +56,45 @@ def background():
             x += 82
         x = 190
         y += 82
-position(X_cat, Y_cat)
+def end():
+    pygame.draw.rect(display_surface,(0,255,0),[600,353,80,80])
 background()
-position(x_cm, y_cm)
-
-for k in range(5):
+pygame.draw.rect(display_surface, red, [X_cat, Y_cat, 80, 80])
+end()
+for k in range(20):
     RM = randint(1, 4)
-
-    if x_cm == X_cat and y_cm == Y_cat:
+    if (X_m < 200 or X_m > 520) or (Y_m < 200 or Y_m > 520):
+        delay()
+        break
+    if X_m == X_cat and Y_m == Y_cat:
+        delay()
+        break
+    if (X_m < 680 or X_m > 520) and (Y_m < 400 or Y_m > 520):
         delay()
         break
 
     if RM == 1:
-        x_cm += 82
+        X_m += 82
         Draw_m()
 
     elif RM == 2:
-        x_cm -= 82
+        X_m -= 82
         Draw_m()
 
     elif RM == 3:
-        y_cm += 82
+        Y_m += 82
         Draw_m()
 
     elif RM == 4:
-        y_cm -= 82
+        Y_m -= 82
         Draw_m()
 
 
-end = pygame.draw.rect(display_surface,(0,255,0),[600,353,80,80])
-
 
 while True:
-
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
         pygame.display.update()
-
