f761064454548f3956920598f0c0eb7f7162a158
Mohanadawad12 <88785140+Mohanadawad12@users.noreply.github.com>
2021-08-15 18:48:35 +0200

    interior design

--- final project - team 7.py
+++ final project - team 7.py
@@ -3,20 +3,56 @@ pygame.init()
 black = (0, 0, 0)  
 white = (255, 255, 255)
 height = 800  
-width = 800 
+width = 800
+blue=(0,110,255)
 
 display_surface = pygame.display.set_mode((height, width))  
   
 pygame.display.set_caption('Image')  
-  
 
- 
-while True:  
-    display_surface.fill(black)  
-    pygame.draw.rect(display_surface, white, [100, 100, 605, 605])
+x=107
+y=107
+i=0
 
-    for event in pygame.event.get():  
-        if event.type == pygame.QUIT:  
-            pygame.quit()    
-            quit()  
-        pygame.display.update()   
+
+
+pygame.display.flip()
+display_surface.fill(black)
+pygame.draw.rect(display_surface, blue, [x, y, 80, 80])
+for i in range(7):
+        pygame.draw.rect(display_surface, blue, [x, y, 80, 80])
+        y += 82
+x=600
+y=107
+for i in range(7):
+        pygame.draw.rect(display_surface, blue, [x, y, 80, 80])
+        y += 82
+x=190
+y=107
+for i in range(5):
+    pygame.draw.rect(display_surface, blue, [x, y, 80, 80])
+    x+= 82
+x=190
+y=600
+for i in range(5):
+    pygame.draw.rect(display_surface, blue, [x, y, 80, 80])
+    x += 82
+x=190
+y=190
+for i in range (5):
+
+    for i in range (5):
+        pygame.draw.rect(display_surface, white, [x,y,80,80])
+        x+=82
+    x=190
+    y+=82
+
+
+while True:
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        pygame.display.update()
