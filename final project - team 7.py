a7741420bb97c5f0e4aabe406474bf263e4e38c3
Ibrahim-Hossam <himaswim@gmail.com>
2021-08-15 20:41:56 +0200

    Added Cat motion

--- final project - team 7.py
+++ final project - team 7.py
@@ -1,11 +1,14 @@
 import pygame
+from random import randint
 pygame.init()
 black = (0, 0, 0)
 white = (255, 255, 255)
+red = (255 , 0, 0)
 height = 800
 width = 800
 blue = (0, 110, 255)
-
+X_cat = (randint(1,5)*82)+107
+Y_cat = (randint(1,5)*82)+107
 display_surface = pygame.display.set_mode((height, width))
 
 pygame.display.set_caption('mouse and cat')
@@ -16,7 +19,6 @@ x = 107
 y = 107
 i = 0
 
-pygame.display.flip()
 display_surface.fill(black)
 pygame.draw.rect(display_surface, blue, [x, y, 80, 80])
 for i in range(7):
@@ -41,12 +43,12 @@ x = 190
 y = 190
 for i in range(5):
 
-    for i in range(5):
-        pygame.draw.rect(display_surface, white, [x, y, 80, 80])
+    for j in range(5):
+        pygame.draw.rect(display_surface, white , [x, y, 80, 80])
         x += 82
     x = 190
     y += 82
-
+pygame.draw.rect(display_surface, red, [X_cat, Y_cat, 80, 80])
 while True:
 
     for event in pygame.event.get():
