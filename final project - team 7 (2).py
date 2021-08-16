921ec7473576c65b11a24d26c4f1e6c195089012
AhmedSayed54 <51422087+AhmedSayed54@users.noreply.github.com>
2021-08-16 14:51:09 +0200

    bonus

--- final project - team 7 (2).py
+++ final project - team 7 (2).py
@@ -1,4 +1,3 @@
-# Initializing variables
 import pygame
 from random import randint
 pygame.init()
@@ -14,8 +13,13 @@ Y_cat = (randint(1,5)*82)+108
 X_m = (randint(1,5)*82)+108
 Y_m = (randint(1, 5) * 82) + 108
 grey = (190, 190, 190)
+ctr = 0
+
+
+
 display_surface = pygame.display.set_mode((height, width))
 pygame.display.set_caption('mouse and cat')
+
 def Draw_m():
     pygame.draw.rect(display_surface, grey, [X_m, Y_m, 80, 80])
 def delay():
@@ -58,43 +62,83 @@ def background():
         y += 82
 def end():
     pygame.draw.rect(display_surface,(0,255,0),[600,353,80,80])
+def scenarios():
+    if X_m == X_cat and Y_m == Y_cat:
+        quit()
+    elif ctr == 20:
+        quit()
+    elif X_m == 600 and Y_m == 354:
+        quit()
+    elif X_m == 600 or Y_m == 108 or X_m == 108 or Y_m == 600:
+        quit()
+
 background()
 pygame.draw.rect(display_surface, red, [X_cat, Y_cat, 80, 80])
 end()
-for k in range(20):
-    RM = randint(1, 4)
-    if (X_m < 200 or X_m > 520) or (Y_m < 200 or Y_m > 520):
-        delay()
-        break
-    if X_m == X_cat and Y_m == Y_cat:
-        delay()
-        break
-    if (X_m < 680 or X_m > 520) and (Y_m < 400 or Y_m > 520):
-        delay()
-        break
 
-    if RM == 1:
-        X_m += 82
-        Draw_m()
-
-    elif RM == 2:
-        X_m -= 82
-        Draw_m()
-
-    elif RM == 3:
-        Y_m += 82
-        Draw_m()
-
-    elif RM == 4:
-        Y_m -= 82
-        Draw_m()
-
-
-
+Draw_m()
 while True:
 
+    keys_pressed = pygame.key.get_pressed()
+
+    if keys_pressed[pygame.K_LEFT]:
+        X_m -= 82
+        Draw_m()
+        delay()
+        ctr += 1
+    
+    if keys_pressed[pygame.K_RIGHT]:
+        X_m += 82
+        Draw_m()
+        delay()
+        ctr += 1
+
+    if keys_pressed[pygame.K_UP]:
+        Y_m -= 82
+        Draw_m()
+        delay()
+        ctr += 1
+
+    if keys_pressed[pygame.K_DOWN]:
+        Y_m += 82
+        Draw_m()
+        delay()
+        ctr += 1
+    scenarios()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
         pygame.display.update()
+
+    
+
+    '''
+    #random motion:
+    for i in range(7):
+        pygame.draw.rect(display_surface, blue, [x, y, 80, 80])
+        y += 82
+    x = 600
+    y = 107
+    for i in range(7):
+        pygame.draw.rect(display_surface, blue, [x, y, 80, 80])
+        y += 82
+    x = 190
+    y = 107
+    for i in range(5):
+        pygame.draw.rect(display_surface, blue, [x, y, 80, 80])
+        x += 82
+    x = 190
+    y = 600
+    for i in range(5):
+        pygame.draw.rect(display_surface, blue, [x, y, 80, 80])
+        x += 82
+    x = 190
+    y = 190
+    for i in range(5):
+
+        for i in range(5):
+            pygame.draw.rect(display_surface, white, [x, y, 80, 80])
+            x += 82
+        x = 190
+        y += 82'''
